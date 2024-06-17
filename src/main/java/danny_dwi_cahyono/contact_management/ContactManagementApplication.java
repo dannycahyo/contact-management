54e697539f192ab1288a9761dc91958072acb823
dannycahyo <dannydwicahyono@gmail.com>
2024-06-17 14:08:48 +0700

    first commit

---
+++ src/main/java/danny_dwi_cahyono/contact_management/ContactManagementApplication.java
@@ -0,0 +1,12 @@
+package danny_dwi_cahyono.contact_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/danny_dwi_cahyono/contact_management/ContactManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package danny_dwi_cahyono.contact_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
