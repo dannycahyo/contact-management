b900d0b16d252f1d1ad6a93c565a51d272e8c32b
dannycahyo <dannydwicahyono@gmail.com>
2024-06-17 14:09:33 +0700

    feat: Add User entity with username, password, name, token, and tokenExpiredAt fields

---
+++ src/main/java/danny_dwi_cahyono/contact_management/entity/User.java
@@ -0,0 +1,28 @@
+package danny_dwi_cahyono.contact_management.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+class User {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String token;
+
+    @Column(name = "token_expired_at")
+    private Long tokenExpiredAt;
+}
