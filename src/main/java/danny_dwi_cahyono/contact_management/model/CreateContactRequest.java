48276bd7dddbc34a3f8a6cfea665b38e33d21a11
dannycahyo <dannydwicahyono@gmail.com>
2024-06-22 19:29:08 +0700

    refactor: Add validation annotations to CreateContactRequest fields

--- src/main/java/danny_dwi_cahyono/contact_management/model/CreateContactRequest.java
+++ src/main/java/danny_dwi_cahyono/contact_management/model/CreateContactRequest.java
@@ -1,5 +1,7 @@
 package danny_dwi_cahyono.contact_management.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,11 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CreateContactRequest {
+    @NotBlank
     private String firstName;
 
+    @Size(min = 1, max = 50)
     private String lastName;
 
+    @Size(min = 1, max = 50)
     private String email;
 
+    @Size(min = 1, max = 50)
     private String phone;
 }
